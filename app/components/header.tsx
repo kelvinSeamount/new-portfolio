8f8a31f47e241d278eac7ab978ed5ca11757ffd1
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2024-05-15 00:33:51 +0200

    header animation

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -15,9 +15,11 @@ const Header = () => {
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-slate-500 sm:gap-5 sm:flex-nowrap sm:w-[initial]">
           {links.map((link) => (
-            <li
+            <motion.li
               key={link.hash}
               className="h-3/4 flex items-center justify-center"
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
             >
               <Link
                 href={link.hash}
@@ -25,7 +27,7 @@ const Header = () => {
               >
                 {link.name}
               </Link>
-            </li>
+            </motion.li>
           ))}
         </ul>
       </nav>
