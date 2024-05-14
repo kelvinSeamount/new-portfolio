9b89c62ee8d537cecd7db1fd3d63ee13412a4c31
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2024-05-15 00:31:56 +0200

    add header

---
+++ app/components/header.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import { links } from "../lib/data";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const Header = () => {
+  return (
+    <header className="relative z-[999]">
+      <motion.div
+        className="fixed top-0 left-1/2  h-[4.5rem] w-full rounded-none border border-opacity-40 border-white bg-white bg-opacity-80 sm:top-6 sm:h-[52px] sm:w-[576px] sm:rounded-full shadow-lg shadow-black/[0.03] backdrop-blur-[8px]"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      ></motion.div>
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-slate-500 sm:gap-5 sm:flex-nowrap sm:w-[initial]">
+          {links.map((link) => (
+            <li
+              key={link.hash}
+              className="h-3/4 flex items-center justify-center"
+            >
+              <Link
+                href={link.hash}
+                className="flex w-full items-center px-3 py-3 hover:text-slate-950 transition"
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "./components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,9 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={`${inter.className}bg-gray-50 text-gray-950`}>
+      <body
+        className={`${inter.className}bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}
+      >
         <div className="absolute top-[-6rem] right-[11rem] bg-[#fbe2e3] h-[31.25rem] blur-[5rem] sm:w-[69rem] -z-10"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] rounded-full w-[50rem] h-[31.25rem] blur-[5rem] sm:w-[69rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] -z-10"></div>
+        <Header />
         {children}
       </body>
     </html>
