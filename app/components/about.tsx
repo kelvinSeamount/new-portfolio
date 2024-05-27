7889f70f1293911cd5865c17c9085a084acab4ef
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2024-05-28 01:39:43 +0200

    refactor error

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -17,16 +17,16 @@ const About = () => {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        {"I'm"} a {""}
+        I&aposm a {""}
         <span className="font-bold">
           geologist-turned-frontend developer
         </span>{" "}
         passionate about sustainability and creativity, dedicated to
         <span className="font-bold">building eco-friendly web solutions</span>.
-        Outside of coding, I enjoy video games and running .Currently, {"I'm"}
+        Outside of coding, I enjoy video games and running .Currently, I&aposm
         improving my backend skills, focusing on
         <span className="italic font-bold"> Node.js.</span> {""}
-        {"I'm"} actively seeking a full-time software developer role and would
+        I&aposm actively seeking a full-time software developer role and would
         love to collaborate and create something great together! 🙌
       </p>
     </motion.section>
