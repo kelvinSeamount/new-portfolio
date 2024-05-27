6abfb7d0fe5e46460e2928a6a6e808e478cea5f2
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2024-05-28 01:27:59 +0200

    refactor

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -17,17 +17,17 @@ const About = () => {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        I'm a {""}
+        {"I'm"} a {""}
         <span className="font-bold">
           geologist-turned-frontend developer
         </span>{" "}
         passionate about sustainability and creativity, dedicated to
         <span className="font-bold">building eco-friendly web solutions</span>.
-        Outside of coding, I enjoy video games and running .Currently, I'm
+        Outside of coding, I enjoy video games and running .Currently, {"I'm"}
         improving my backend skills, focusing on
         <span className="italic font-bold"> Node.js.</span> {""}
-        I'm actively seeking a full-time software developer role and would love
-        to collaborate and create something great together! 🙌
+        {"I'm"} actively seeking a full-time software developer role and would
+        love to collaborate and create something great together! 🙌
       </p>
     </motion.section>
   );
