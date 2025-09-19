927b604285249a9e519d45e3f206c0db50d6481a
kelvinSeamount <kelvinnwachukwu89@gmail.com>
2025-09-19 14:38:43 +0200

    update const data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,5 @@
 import EatnslipImg from "@/public/Eatnslip.png";
+import DashboardImg from "@/public/Dashboard.png";
 
 export const links = [
   {
@@ -47,5 +48,14 @@ export const projectData = [
       "The name of the app is called eat-n-slip it's an application that lets you basically to split bills with your friends when you go out be it for lunch or dinner. the application has functionality of adding friends, showing you also how much your friends are owing you and vice versa.",
     tags: ["React", "CSS", "JavaScript"],
     imageUrl: EatnslipImg,
+    projectUrl: "https://eat-n-slip.vercel.app"
+  },
+   {
+    title: "Dashboard",
+    description:
+      "Observability Dashboard, A high-performance React observability platform that delivers real-time monitoring,logging,metrics visualization, and distributed tracing for microservices,enabling teams to troubleshoot swiftly and understand their systems deeply.",
+    tags: ["React","JavaScript","TypeScript","React Query","Tailwind"],
+    imageUrl: DashboardImg,
+    projectUrl: "https://kelvinseamount.github.io/Observability-Dashboard/"
   },
 ];
